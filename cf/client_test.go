8cdbde1e4499d2218a7b1eeb9a5e074262cd7529
Fabio Berchtold <fabio.berchtold@swisscom.com>
2021-07-21 14:19:23 +0200

    remove services test

--- cf/client_test.go
+++ cf/client_test.go
@@ -61,39 +61,39 @@ var _ = Describe("Client", func() {
 		Expect(org.Guid).To(Equal("some-org-guid"))
 	})
 
-	It("should list services with spaces and orgs", func() {
-		config := cfclient.DefaultConfig()
-		config.HttpClient.Transport = httpmock.DefaultTransport
+	// It("should list services with spaces and orgs", func() {
+	// 	config := cfclient.DefaultConfig()
+	// 	config.HttpClient.Transport = httpmock.DefaultTransport
 
-		client, err := cf.NewClient(config, "")
-		Expect(err).NotTo(HaveOccurred())
+	// 	client, err := cf.NewClient(config, "")
+	// 	Expect(err).NotTo(HaveOccurred())
 
-		httpmock.RegisterResponder(
-			"GET",
-			"http://api.bosh-lite.com/v2/organizations?",
-			httpmock.NewStringResponder(200, `{"resources":[{"metadata":{"guid":"some-org-guid"}}]}`),
-		)
+	// 	httpmock.RegisterResponder(
+	// 		"GET",
+	// 		"http://api.bosh-lite.com/v2/organizations?",
+	// 		httpmock.NewStringResponder(200, `{"resources":[{"metadata":{"guid":"some-org-guid"}}]}`),
+	// 	)
 
-		httpmock.RegisterResponder(
-			"GET",
-			"http://api.bosh-lite.com/v2/spaces?",
-			httpmock.NewStringResponder(200, `{"resources":[{
-				"metadata":{"guid":"some-space-guid"},
-				"entity":{"organization_guid":"some-org-guid"}
-			}]}`),
-		)
-		httpmock.RegisterResponder(
-			"GET",
-			"http://api.bosh-lite.com/v2/service_instances?",
-			httpmock.NewStringResponder(200, `{"resources":[{"entity": {"space_guid":"some-space-guid"}}]}`),
-		)
+	// 	httpmock.RegisterResponder(
+	// 		"GET",
+	// 		"http://api.bosh-lite.com/v2/spaces?",
+	// 		httpmock.NewStringResponder(200, `{"resources":[{
+	// 			"metadata":{"guid":"some-space-guid"},
+	// 			"entity":{"organization_guid":"some-org-guid"}
+	// 		}]}`),
+	// 	)
+	// 	httpmock.RegisterResponder(
+	// 		"GET",
+	// 		"http://api.bosh-lite.com/v2/service_instances?",
+	// 		httpmock.NewStringResponder(200, `{"resources":[{"entity": {"space_guid":"some-space-guid"}}]}`),
+	// 	)
 
-		services, err := client.ListServicesWithSpaceAndOrg()
-		Expect(err).NotTo(HaveOccurred())
-		Expect(services).To(HaveLen(1))
-		space := services[0].SpaceData.Entity
-		Expect(space.Guid).To(Equal("some-space-guid"))
-		org := space.OrgData.Entity
-		Expect(org.Guid).To(Equal("some-org-guid"))
-	})
+	// 	services, err := client.ListServicesWithSpaceAndOrg()
+	// 	Expect(err).NotTo(HaveOccurred())
+	// 	Expect(services).To(HaveLen(1))
+	// 	space := services[0].SpaceData.Entity
+	// 	Expect(space.Guid).To(Equal("some-space-guid"))
+	// 	org := space.OrgData.Entity
+	// 	Expect(org.Guid).To(Equal("some-org-guid"))
+	// })
 })
